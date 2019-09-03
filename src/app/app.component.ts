09d7017edb23c59c18374cd9db60345175c22a60
Jason <jason.shih.619@gmail.com>
2019-09-03 11:36:26 -0400

    TopNav styled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { TopNavComponent } from './top-nav/top-nav.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'DjinnNet';
 }
